a3f3a804142bcedb5fd9e88f6eee1ce09d7fb67f
transcendentalcat <ninkaperegon@gmail.com>
2018-07-10 01:11:50 +0300

    Added PhotoController for infinity scrolling

---
+++ IlyaSnigirPhotographer/Controllers/PhotoController.cs
@@ -0,0 +1,49 @@
+﻿using IlyaSnigirPhotographer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IlyaSnigirPhotographer.Controllers
+{
+    public class PhotoController : Controller
+    {
+        PortfolioDbContext db = new PortfolioDbContext();
+        const int recordsPerPage = 8;
+
+        public ActionResult Index(int? id)
+        {
+            var page = id ?? 0;
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_Photos", GetPaginatedPhotos(page));
+            }
+
+            return View("Index", db.Photos.Take(recordsPerPage));
+        }
+
+        public ActionResult Product(int? id)
+        {
+            var page = id ?? 0;
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_Photos", GetPaginatedPhotos(page));
+            }
+
+            return View("Index", db.Photos.Take(recordsPerPage));
+        }
+
+        private List<Photo> GetPaginatedPhotos(int page = 1)
+        {
+            var skipRecords = page * recordsPerPage;
+
+            return db.Photos
+                .OrderBy(x => x.CreatedDate)
+                .Skip(skipRecords)
+                .Take(recordsPerPage).ToList();
+        }
+    }
+}
