ab2eb989543173e3da13f2e29a583b8230c1d495
transcendentalcat <ninkaperegon@gmail.com>
2018-07-21 03:31:52 +0300

    Swap Contacts and About views. Remove About

--- IlyaSnigirPhotographer/Controllers/HomeController.cs
+++ IlyaSnigirPhotographer/Controllers/HomeController.cs
@@ -22,14 +22,8 @@ namespace IlyaSnigirPhotographer.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
-
-        public ActionResult Contact()
+        public ActionResult Contacts()
         {
             ViewBag.Message = "Your contact page.";
 
