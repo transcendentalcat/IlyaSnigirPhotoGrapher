81fb769911d369738540aa83aadec4c97cc85e9c
transcendentalcat <ninkaperegon@gmail.com>
2018-08-15 16:41:17 +0300

    Added  up-button to photos

--- IlyaSnigirPhotographer/Scripts/script.js
+++ IlyaSnigirPhotographer/Scripts/script.js
@@ -91,6 +91,22 @@ $(document).ready(function () {
 var allImages = document.querySelectorAll(".album .album-item img");
 var currentImage;
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("btn-up").style.display = "block";
+    } else {
+        document.getElementById("btn-up").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 /*$(window).on('scroll', function(){
 	if($(window).scrollTop()) {
