8460e196628f26654c4ada90aa472ca1e685645a
transcendentalcat <ninkaperegon@gmail.com>
2018-08-15 17:10:39 +0300

    Up-button added to albums

--- IlyaSnigirPhotographer/Scripts/script.js
+++ IlyaSnigirPhotographer/Scripts/script.js
@@ -91,22 +91,19 @@ $(document).ready(function () {
 var allImages = document.querySelectorAll(".album .album-item img");
 var currentImage;
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
+// Up-button
+$(window).on("scroll",
+    function () {
+        if ($(window).scrollTop() > 200) {
+            $(".btn-up").fadeIn();
+        } else {
+            $(".btn-up").fadeOut();
+        }
+    });
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("btn-up").style.display = "block";
-    } else {
-        document.getElementById("btn-up").style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+$(".btn-up").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+});
 
 /*$(window).on('scroll', function(){
 	if($(window).scrollTop()) {
